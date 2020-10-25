c17961e5055c17f1c393efbb8a9c00fff4ce79a2
wilvolcy <50376806+wilvolcy@users.noreply.github.com>
2020-10-24 22:43:41 -0500

    more test with unroll explanation

--- src/test/groovy/com/example/studentgrade/ComputeMaxSpec.groovy
+++ src/test/groovy/com/example/studentgrade/ComputeMaxSpec.groovy
@@ -1,8 +1,10 @@
 package com.example.studentgrade
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class ComputeMaxSpec extends Specification{
+    @Unroll ("The maximum of #a and #b is #c")
     def 'Compute maximum of two number '(){
         expect :
             Math.max(a ,b) == c
@@ -10,7 +12,15 @@ class ComputeMaxSpec extends Specification{
             a << [1, 3]
             b << [3, 4]
             c << [3, 4]
-
+    }
+    @Unroll
+    def 'compute maximum of 2 number using table'(){
+        expect :
+            Math.max(num1,num2) == maxValue
+        where :
+        maxValue | num1 | num2
+        5        | 3    |5
+        9        | 6    |9
     }
     def "Test with stack "(){
         given :
@@ -34,4 +44,32 @@ class ComputeMaxSpec extends Specification{
             e.cause == null
 
     }
+    def 'check notThrown exception for HasMap' () {
+        given :
+            def mapValue = new HashMap<String,String>()
+        when :
+            mapValue.put(null,"anything")
+        then:
+        notThrown(NullPointerException)
+    }
+   def 'Check old at listing' (){
+       given :
+            def values = ['3','2','6']
+       when :
+            values << 9
+       then :
+            values.size() == 4
+            old(values.size()) == 3
+   }
+   def 'Check old value with map' (){
+       given :
+            def map = [:]
+            map.example = 'some value'
+       when :
+            map.example = 'change value'
+       then :
+            map.example == 'change value'
+            old(map.example) == 'some value'
+            old(map.example) != map.example
+   }
 }
